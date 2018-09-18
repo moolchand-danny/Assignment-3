8103ae916cfd696a0ecaa02e4983974d8080fbe7
Danny Moolchand <dannyrmoolchand@ufl.edu>
2018-09-17 23:59:14 -0400

    Updated config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    db: {
+        uri: 'mongodb://CEN3031:CEN3031TA@ds117539.mlab.com:17539/assignment3_mongodb', //place the URI of your mongo database here.
+    }
+};
+    //updated with the CEN3031 user and pass
+  /* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() */
